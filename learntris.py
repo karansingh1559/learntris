7d6c937930c077035301c70a2bae354cd9874e5d
karansingh1559 <karansingh1559@gmail.com>
2018-04-07 03:47:46 +0530

    Test 8 passed
    Was, making a silly mistake by not assigning a list to the list
    comprehension. PRO-TIP: you can access the row of a 2-D matrix by index,
    like tetramino[2]

--- learntris.py
+++ learntris.py
@@ -7,12 +7,14 @@ matrix=[['.' for x in range(WIDTH)] for y in range(HEIGHT)]
 score=0
 ncl=0       #number of cleared lines
 emptyRow= ['.' for x in range(WIDTH)]
+tetraminoList=['I']
+baseTetraminoMatrix=[['.' for x in range(4)] for y in range(4)]
+tetramino=[['.' for x in range(4)] for y in range(4)]
+#tetraminoIndex={ 'I':1}
 
-
-def printMatrix():
-    global matrix
-    for y in range(HEIGHT):
-        for x in range(WIDTH-1):
+def printMatrix(matrix):
+    for y in range(len(matrix)):
+        for x in range(len(matrix[0])-1):
             print(matrix[y][x], end=' ')
         print(matrix[y][x+1], end='\n')
 
@@ -35,13 +37,18 @@ def step():
             row[:] = emptyRow
             ncl=ncl+1
             score=score+100
-    
+
+def generateTetramino(tetraminoIndex):
+    global tetramino
+    if tetraminoIndex==1:
+        tetramino[1] = ['c' for elem in tetramino[1]]
+
 while(True):
     inp = raw_input()
     if (inp=='q'):
         break
     elif inp=='p':
-        printMatrix()
+        printMatrix(matrix)
     elif inp=='g':
         saveMatrix()
     elif inp=='c':
@@ -52,4 +59,7 @@ while(True):
         print(ncl)
     elif inp=='s':
         step()
-
+    elif inp=='t':
+        printMatrix(tetramino)
+    elif inp=='I':
+        generateTetramino(1)
